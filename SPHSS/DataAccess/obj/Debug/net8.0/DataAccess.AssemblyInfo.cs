d2db5d31860039c1657ee7f39699bb155960df4f
MinhHoang0204 <hoangtmse151422@fpt.edu.vn>
2025-04-04 20:35:10 +0700

    ?

--- SPHSS/DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ SPHSS/DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaa2d9ebaa80af269e2cb42eb04f4b72376bb7d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fecc5df37690503973f878f1d15641daa386fb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
